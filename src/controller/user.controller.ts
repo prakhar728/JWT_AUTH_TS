c1a7515e30d4f79bd3a121f7985b5647a0d02fbc
Prakhar Ojha <prakhar@getStan.app>
2022-09-12 10:56:21 +0530

    User can be verified now

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { CreateUserInput } from "../schema/user.schema";
-import { createUser } from "../service/user.service";
+import { CreateUserInput, VerifyUserInput } from "../schema/user.schema";
+import { createUser, findUserById } from "../service/user.service";
 import sendEmail from '../utils/mailer';
 export async function createUserHandler(req:Request<{},{},CreateUserInput>,res:Response){
     const body = req.body;
@@ -22,4 +22,32 @@ export async function createUserHandler(req:Request<{},{},CreateUserInput>,res:R
 
         return res.status(500).send(e)
     }
+}
+
+export async function verifyUserHandler(req:Request<VerifyUserInput>,res:Response){
+    const id = req.params.id;
+    const verificationCode = req.params.verificationCode;
+
+
+    //find the user by id
+    const user = await findUserById(id);
+    if(!user)
+    return res.send('Could not verify User');
+
+    //check to see if already verified
+
+    if(user.verified)
+    return res.send('user is already verified');
+    //check if the verification code matches
+
+    if(user.verificationCode == verificationCode){
+        user.verified=true;
+
+        await user.save();
+        return res.send("User successfully Verified");
+    }
+
+    return res.send("Could not verify User");
+
+
 }
