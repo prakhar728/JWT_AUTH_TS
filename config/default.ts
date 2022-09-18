fd4d8d93fe23e9e4d4140863ecc67b8961929e5c
Prakhar Ojha <prakhar@getStan.app>
2022-09-18 10:23:00 +0530

    Finalized

---
+++ config/default.ts
@@ -0,0 +1,14 @@
+export default {
+    port:3000,
+    dbURI:"",
+    loglevel:"info",
+    accessTokenPrivateKey:"",
+    refreshTokenPrivateKey:"",
+    smtp:{
+        user: 'maverick.gibson@ethereal.email',
+        pass: '76S7UCzE5M261RXC8m',
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure:false
+    }
+}
